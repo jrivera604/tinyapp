87dde9ea483dbb6264ef364a12df3aafff563cda
jrivera604 <joshrivera91@hotmail.com>
2022-12-01 08:06:57 +0000

    add delete button

--- express_server.js
+++ express_server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,8 +27,17 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  let id= generateRandomString();
+  urlDatabase[id] = {longURL: req.body.longURL}
+  res.redirect(`/urls/${id}`)
+
+});
+
+
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { id: req.params.id, urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
@@ -35,14 +45,33 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL:  urlDatabase};
+  const templateVars = { id: req.params.id, longURL: urlDatabase};
+  console.log(templateVars)
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  res.redirect(longURL).full;
 });
 
-function generateRandomString() {}
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id]
+  res.redirect("/urls");
+});
+
+
+
+
+
+const generateRandomString = () => {
+  let shortUrl = [];
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  for (let i = 0; i <=5; i++) {
+    shortUrl.push(characters.charAt(Math.floor(Math.random()* characters.length)));
+  }
+  return shortUrl.join('');
+}
