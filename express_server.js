674980cef2321a078a42d9bea97f6dd74e71cfb7
jrivera604 <joshrivera91@hotmail.com>
2022-12-03 07:18:27 +0000

    add logout function

--- express_server.js
+++ express_server.js
@@ -48,8 +48,13 @@ app.get("/login", (req, res) => {
 })
 
 app.post("/login" , (req, res) => {
-  res.cookie('username', req.body.username)
+  res.cookie("username", req.body.username)
   res.redirect("/urls")
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
@@ -62,17 +67,18 @@ app.post("/urls", (req, res) => {
 
 
 app.get("/urls", (req, res) => {
-  const templateVars = { id: req.params.id, urls: urlDatabase};
+  const templateVars = { id: req.params.id, urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase};
+  const templateVars = { id: req.params.id, longURL: urlDatabase, username: req.cookies["username"]};
   console.log(templateVars)
   res.render("urls_show", templateVars);
 });
